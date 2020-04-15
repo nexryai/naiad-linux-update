34275019a406d68b0bbafc555c921ef2ea618af6
nexryai <61890205+nexryai@users.noreply.github.com>
2020-04-15 20:50:49 +0900

    Add files via upload

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-file=ver002
+file=ver003
 echo naiad linux update $file
 cd
 cd .naiad-updater
@@ -16,3 +16,4 @@ mkdir Downloads
 gsettings set org.blueman.transfer shared-path $HOME/Downloads/
 sudo rm /var/crash/*
 sudo sed -i 's/enabled=1/enabled=0/g' /etc/default/apport
+sudo apt purge ubuntu-report popularity-contest apport whoopsie
